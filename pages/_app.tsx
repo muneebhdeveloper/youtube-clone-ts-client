60c7c2e4f9ffc6b0acd6305f8c023fe7c00b10c8
Muneeb Hussain Modi <muneebhussainmodi@gmail.com>
2022-04-24 03:25:11 +0500

    first commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,40 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import { AppProps } from "next/app";
+import Head from "next/head";
+import { MantineProvider } from "@mantine/core";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NotificationsProvider } from "@mantine/notifications";
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+const queryClient = new QueryClient();
+
+export default function App(props: AppProps) {
+  const { Component, pageProps } = props;
+
+  return (
+    <>
+      <Head>
+        <title>Functional Youtube Clone</title>
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width"
+        />
+      </Head>
+
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{
+          /** Put your mantine theme override here */
+          colorScheme: "light",
+        }}
+      >
+        <QueryClientProvider client={queryClient}>
+          <NotificationsProvider>
+            <main>
+              <Component {...pageProps} />
+            </main>
+          </NotificationsProvider>
+        </QueryClientProvider>
+      </MantineProvider>
+    </>
+  );
 }
-
-export default MyApp
